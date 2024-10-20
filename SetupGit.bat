b2a93f3a23b7094e2c95da3501a4df98bc27f109
greenalden <alden4le@gmail.com>
2024-10-20 19:53:34 -0400

    Git setup

--- SetupGit.bat
+++ SetupGit.bat
@@ -25,11 +25,5 @@ if %errorlevel% equ 0 (
 ) else (
     echo Git installation failed.
 )
-REM Download and install Vosk model
-echo Downloading Vosk model...
-curl -L -O https://alphacephei.com/vosk/models/vosk-model-en-us-0.22.zip
-echo Unzipping Vosk model...
-tar -xvzf vosk-model-en-us-0.22.zip
-del vosk-model-en-us-0.22.zip
-git lfs install
-git clone https://huggingface.co/meta-llama/Llama-3.1-8B-Instruct
+
+pause
