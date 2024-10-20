9cb81d6cda44c4937d13a784788a177fbd08b9cf
greenalden <alden4le@gmail.com>
2024-10-20 17:29:02 -0400

    more Setup stuff

--- Setup3.bat
+++
@@ -1,2 +0,0 @@
-git lfs install
-git clone https://huggingface.co/meta-llama/Llama-3.1-8B-Instruct
--- SetupAIModels.bat
+++ SetupAIModels.bat
@@ -1,6 +1,9 @@
+@echo off
 REM Download and install Vosk model
 echo Downloading Vosk model...
 curl -L -O https://alphacephei.com/vosk/models/vosk-model-en-us-0.22.zip
 echo Unzipping Vosk model...
 tar -xvzf vosk-model-en-us-0.22.zip
 del vosk-model-en-us-0.22.zip
+git lfs install
+git clone https://huggingface.co/meta-llama/Llama-3.1-8B-Instruct
