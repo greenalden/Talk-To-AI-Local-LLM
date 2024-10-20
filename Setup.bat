72f5afe529fe5ead194ec4b39e7d0176e74b8543
greenalden <alden4le@gmail.com>
2024-10-20 14:37:35 -0400

    Setup.bat doing pip installs now

--- Setup.bat
+++ Setup.bat
@@ -41,28 +41,26 @@ if errorlevel 1 (
 :: Force install the specified Python version in the base environment
 echo Installing Python 3.11.9 in the base environment...
 call conda install python=3.11.9 -y
+
+
 python -m pip install --upgrade pip
 
 
 
 pip install numpy
 pip install pandas
-pip install matplotlib
-timeout /t 1
-pip install transformers==4.45.1
-timeout /t 1
-pip install pydub==0.25.1
-timeout /t 1
-pip install vosk==0.3.45
-timeout /t 1
-pip install sounddevice==0.5.1
-timeout /t 1
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-timeout /t 1
 pip install --upgrade pip setuptools wheel
 pip install build
 pip cache purge
-pip install TTS==0.22.0
+pip install TTS==0.22.0 "transformers==4.45.1" "setuptools==75.1.0" "jieba==0.42.1" "encodec==0.1.1" "gruut_lang_de==2.0.1" "gruut_lang_en==2.0.1" "gruut_lang_es==2.0.1" "gruut_lang_fr==2.0.2" "contourpy==1.3.0" "spacy==3.7.6" 
+pip install matplotlib==3.9.2
+pip install setuptools==75.1.0
+pip install transformers==4.45.1
+pip install pydub==0.25.1
+pip install vosk==0.3.45
+pip install sounddevice==0.5.1
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+
 
 
 
